ecfaa97235565ddf62d1b3ae8bceecbc1f3aad3f
gemss <alex.baoyu@gmail.com>
2013-08-13 18:39:44 +0800

    update Gruntfile.js & package.json

--- dist/ablesky_static_build.js
+++
@@ -1,3 +0,0 @@
-/*! ablesky_static_build - v0.0.0 - 2013-08-08
-* http://www.ablesky.com
-* Copyright (c) 2013 ; Licensed  */
--- dist/ablesky_statices.min.js
+++
@@ -1,2 +0,0 @@
-/*! ablesky_statices - v0.0.0 - 2013-08-06
-* Copyright (c) 2013 ; Licensed  */

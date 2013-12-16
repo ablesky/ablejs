2f3f54e8056325f77b4e49797e40bbfa1ef31a40
gemss <alex.baoyu@gmail.com>
2013-12-16 15:18:42 +0800

    refact ablejs

--- Gruntfile.js
+++ Gruntfile.js
@@ -158,7 +158,6 @@ module.exports = function(grunt) {
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-clean');
 
     // Default task.
     grunt.registerTask('build', ['clean', 'concat', 'optiimg', 'opticss', 'optijs', 'optijsp', 'shell', 'chrono']);
